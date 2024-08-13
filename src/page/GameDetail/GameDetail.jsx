8f014cfd730c0ad676ff6b4d29da835c9c1a52c5
Hyun <hwn123h@naver.com>
2024-08-13 20:19:23 +0900

    design updatE

--- src/page/GameDetail/GameDetail.jsx
+++ src/page/GameDetail/GameDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './GameDetail.style.css';
 import { useGameDetailQuery } from '../../hooks/useGameDetailQuery';
 import { useGameSeriesQuery } from '../../hooks/useGameSeriesQuery';
@@ -10,11 +10,22 @@ import { Container, Row, Col } from 'react-bootstrap';
 const GameDetail = () => {
     const { gameSlug } = useParams();
 
+    const [selectedMenu, setSelectedMenu] = useState('description');
+
+    const handleMenu = (menu) => {
+        setSelectedMenu(menu);
+    }
+
+
     const { data, isLoading, isError, error } = useGameDetailQuery(gameSlug)
     const { data: series } = useGameSeriesQuery(gameSlug)
     const { data: screenshots } = useGameScreenShotQuery(gameSlug)
     const { data: trailer } = useGameTrailerQuery(gameSlug);
-    console.log(data);
+    console.log(series);
+
+    if (!data || !data || data.length === 0) {
+        return <div>No games found.</div>;
+    }
 
     if (!screenshots || !screenshots || screenshots.length === 0) {
         return <div>No games found.</div>;
@@ -22,47 +33,85 @@ const GameDetail = () => {
     return (
 
         <Container className="game-detail">
+            <div className="game-detail-top">
+                <div className="game-detail-left" lg={6} xs={12}>
+                    <h1>{data?.name}</h1>
+                    <img src={data?.background_image} alt={data?.name} width="490" height="450" />
+                </div>
 
-            <div className="game-detail-left" lg={6} xs={12}>
-                <h1>{data?.name}</h1>
-                <img src={data?.background_image} alt={data?.name} width="490" height="450" />
+                <div className="game-detail-right" lg={6} xs={12}>
+                    <h2><span>Game </span>Details</h2>
+                    <div className="game-description">
+                        {(data?.description).split('<br />')[0].replace(/<p>/g, '') || (data?.description).split('<p />')[0].replace(/<p>/g, '')}
+                    </div>
+                    <div className="game-informations">
+                        <span>RELEASE DATE : </span>{data?.released}
+                        <span>Platforms :</span>
+                        <div className="platforms">
+                            {data?.platforms.map(platform => (
+                                <p>{platform.platform.name}</p>
+                            ))}
+                        </div>
+                        <span>Developers :</span>
+                        <div className='developers'>
+                            {data?.developers.map(developer => (
+                                <p>{developer.name}</p>
+                            ))}
+                        </div>
+
+                        <span>Genres :</span>
+                        <div className='genres'>
+                            {data?.genres.map(genre => (
+                                <p>{genre.name}</p>
+                            ))}
+                        </div>
+                        <span>Publishers :</span>
+                        <div className='publishers'>
+                            {data?.publishers.map(publisher => (
+                                <p>{publisher.name}</p>
+                            ))}
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
 
-            <div className="game-detail-right" lg={6} xs={12}>
-                <h2><span>Game </span>Details</h2>
-                <div className="game-description">
-                    {(data?.description).split('<br />')[0].replace(/<p>/g, '')}
+
+            <div className="game-detail-bottom">
+                <ul className="menu">
+                    <li className={selectedMenu === 'description' ? 'menu-item active' : 'menu-item'} onClick={() => handleMenu('description')}>description</li>
+                    <li className={selectedMenu === 'store' ? 'menu-item active' : 'menu-item'} onClick={() => handleMenu('store')}>store</li>
+                    <li className={selectedMenu === 'series' ? 'menu-item active' : 'menu-item'} onClick={() => handleMenu('series')}>series</li>
+                </ul>
+                <div className="game-detail-content">
+                    {selectedMenu === 'description' && (
+                        <div className="game-description">
+                            <p>{data?.description_raw}</p>
+                        </div>
+                    )}
+                    {selectedMenu === 'store' && (
+                        <div>
+                            <ul>
+                                {data?.stores.map(store => (
+                                    <li key={store.id}>{store.store.name}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                    {selectedMenu === 'series' && (
+                        <div>
+                            <ul>
+                                {series?.map(series => (
+                                    <li key={series.id} className="game-series">
+                                        <img src={series.background_image} />
+                                        {series.name}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
                 </div>
-                <div className="game-informations">
-                    <span>RELASE DATE : </span>{data?.released}
-                    <span>Platforms :</span>
-                    <div className="platforms">
-                        {data?.platforms.map(platform => (
-                            <p>{platform.platform.name}</p>
-                        ))}
-                    </div>
-                    <span>Developers :</span>
-                    <div className='developers'>
-                        {data?.developers.map(developer => (
-                            <p>{developer.name}</p>
-                        ))}
-                    </div>
-
-                    <span>Genres :</span>
-                    <div className='genres'>
-                        {data?.genres.map(genre => (
-                            <p>{genre.name}</p>
-                        ))}
-                    </div>
-                    <span>Publishers :</span>
-                    <div className='publishers'>
-                        {data?.publishers.map(publisher => (
-                            <p>{publisher.name}</p>
-                        ))}
-                    </div>
-
-                </div>
-
             </div>
 
         </Container>
