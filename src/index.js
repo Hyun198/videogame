33ed72238c59852e5c86e385a36357f8c207a057
Hyun <hwn123h@naver.com>
2024-10-17 16:43:20 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Provider } from 'react-redux'
-import store from './redux/store';
+
 
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,9 +13,7 @@ root.render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <React.StrictMode>
-        <Provider store={store}>
-          <App />
-        </Provider>
+        <App />
       </React.StrictMode>
     </BrowserRouter>
   </QueryClientProvider>
